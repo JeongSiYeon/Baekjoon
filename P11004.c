e1b823c1ec4546ab32ec49a60471bef25b3e5873
JeongSiYeon <siyeoni0209@gachon.ac.kr>
2022-09-17 21:12:10 +0900

    P11004: changed macro fuction(MAX, MIN -> MAXI, MINI) to compare values rather than indexes

--- P11004.c
+++ P11004.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
 
-#define SWAP(a, b, tmp) {tmp=a; a=b; b=tmp;}   //swap을 define으로 함
-
+#define SWAP(a, b, tmp) {tmp=a; a=b; b=tmp;} 
+#define MAXI(arr, a, b, c, maxi) {maxi = (*(arr+a)>*(arr+b)) ? a:b; if(*(arr+c)>maxi) maxi=c;}    // 인덱스비교가 아니라 값비교로 바꿈
+#define MINI(arr, a, b, c, mini) {mini = (*(arr+a)<*(arr+b)) ? a:b; if(*(arr+c)<mini) mini=c;}
+  
 void quickSort(int *arr, int p, int r);
 int partition(int *arr, int p, int r);
 
@@ -16,7 +17,6 @@ int main()
     for(int i = 0; i < N; i++){
         scanf("%d%*c", &num[i]);                // -10^9 <= num <= 10^9
     }
-
     quickSort(num, 0, N-1);                    // sort num
     printf("%d\n", num[K-1]);                  // print loc: K               
     return 0;
@@ -34,17 +34,16 @@ void quickSort(int *arr, int p, int r)
 
 int partition(int *arr, int p, int r)
 {
-    int i = p-1, j = p;                          // p랑 j랑 재귀적으로 들어오는거 생각 안하고 첫 번째 입력에 한해서만 함
-    int random, pivot, tmp = 0;  
+    int i = p-1, j;         
+    int pivot, tmp;                 
+    int maxi, mini, mediani, s=(p+r)/2;
     
-    srand(time(NULL));
-    if(p-r+1 == 0)                                // zero division생각안함
-        random = p;
-    else
-        random = rand()%(p-r+1)+p;   
+    MAXI(arr, p, r, s, maxi);
+    MINI(arr, p, r, s, mini);
+    mediani = (p+r+s) - maxi - mini;
 
-    pivot = arr[random];                           // 고정 피벗은 최악의 경우로 갈 확률 높음 -> random하게
-    SWAP(arr[random], arr[r], tmp);                // 밑에 알고리즘 바꾸기 귀찮아서 그냥 swap함
+    SWAP(arr[mediani], arr[r], tmp);
+    pivot = arr[r];               
 
     for(j = p; j < r; j++) {                
         if(arr[j] <= pivot) {
